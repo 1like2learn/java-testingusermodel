b620ce055211420ca199a4ea1f568884e947a8c8
1like2learn <ndespained@gmail.com>
2020-08-20 16:24:45 -0700

    MVP met

--- usermodel/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerTest.java
+++ usermodel/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerTest.java
@@ -1,15 +1,139 @@
 package com.lambdaschool.usermodel.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lambdaschool.usermodel.models.Role;
+import com.lambdaschool.usermodel.models.User;
+import com.lambdaschool.usermodel.models.UserRoles;
+import com.lambdaschool.usermodel.models.Useremail;
+import com.lambdaschool.usermodel.services.UserService;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private List<User> userList;
 
     @Before
     public void setUp() throws Exception {
+
+        userList = new ArrayList<>();
+
+        Role r1 = new Role("admin");
+        r1.setRoleid(1);
+        Role r2 = new Role("user");
+        r2.setRoleid(2);
+        Role r3 = new Role("data");
+        r3.setRoleid(3);
+
+        // admin, data, user
+        User u1 = new User("admin",
+            "password",
+            "admin@lambdaschool.local");
+        u1.setUserid(10);
+        u1.getRoles().add(new UserRoles(u1, r1));
+        u1.getRoles().add(new UserRoles(u1, r2));
+        u1.getRoles().add(new UserRoles(u1, r3));
+        u1.getUseremails()
+            .add(new Useremail(u1,
+                "admin@email.local"));
+        u1.getUseremails().get(0).setUseremailid(11);
+        u1.getUseremails()
+            .add(new Useremail(u1,
+                "admin@mymail.local"));
+        u1.getUseremails().get(1).setUseremailid(12);
+
+        userList.add(u1);
+
+        // data, user
+        User u2 = new User("cinnamon",
+            "1234567",
+            "cinnamon@lambdaschool.local");
+        u2.setUserid(20);
+        u2.getRoles().add(new UserRoles(u2, r2));
+        u2.getRoles().add(new UserRoles(u2, r3));
+        u2.getUseremails()
+            .add(new Useremail(u2,
+                "cinnamon@mymail.local"));
+        u2.getUseremails().get(0).setUseremailid(21);
+        u2.getUseremails()
+            .add(new Useremail(u2,
+                "hops@mymail.local"));
+        u2.getUseremails().get(1).setUseremailid(22);
+        u2.getUseremails()
+            .add(new Useremail(u2,
+                "bunny@email.local"));
+        u2.getUseremails().get(2).setUseremailid(23);
+        userList.add(u2);
+
+        // user
+        User u3 = new User("barnbarn",
+            "ILuvM4th!",
+            "barnbarn@lambdaschool.local");
+
+        u3.setUserid(30);
+        u3.getRoles().add(new UserRoles(u3, r2));
+        u3.getUseremails()
+            .add(new Useremail(u3,
+                "barnbarn@email.local"));
+        u3.getUseremails().get(0).setUseremailid(31);
+        userList.add(u3);
+
+        User u4 = new User("puttat",
+            "password",
+            "puttat@school.lambda");
+        u4.setUserid(40);
+        u4.getRoles().add(new UserRoles(u4, r2));
+        userList.add(u4);
+
+        User u5 = new User("misskitty",
+            "password",
+            "misskitty@school.lambda");
+        u5.setUserid(50);
+        u5.getRoles().add(new UserRoles(u5, r2));
+        userList.add(u5);
+
+//        for(User u: userList){
+//
+//            System.out.println(u.getUsername() + " " + u.getUserid());
+//        }
+
+        //        10 admin
+        //        20 cinnamon
+        //        30 barnbarn
+        //        40 puttat
+        //        50 misskitty
     }
 
     @After
@@ -17,27 +141,142 @@ public class UserControllerTest {
     }
 
     @Test
-    public void listAllUsers() {
+    public void listAllUsers() throws Exception{
+        String apiUrl = "/users/users";
+
+        Mockito.when(userService.findAll()).thenReturn(userList);
+
+        for(User u: userList){
+
+            System.out.println(u.getUsername() + " " + u.getUserid());
+        }
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl)
+            .accept(MediaType.APPLICATION_JSON);
+        System.out.println("rb = apiUrl thing");
+        MvcResult r = mockMvc.perform(rb).andReturn();
+        System.out.println(r + "mockMvc result");
+        String tr = r.getResponse().getContentAsString();
+        System.out.println(tr + "true result");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList);
+        System.out.println(er + "expected result");
+
+        System.out.println("Expect: " + er);
+        System.out.println("Actual: " + tr);
+
+        assertEquals(er, tr);
     }
 
     @Test
-    public void getUserById() {
+    public void getUserById() throws Exception{
+        String apiUrl = "/users/user/20";
+
+        Mockito.when(userService.findUserById(20))
+            .thenReturn(userList.get(1));
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb).andReturn();
+        String tr = r.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList.get(1));
+
+        assertEquals("Rest API Returns List", er, tr);
     }
 
     @Test
-    public void getUserByName() {
+    public void getUserByName() throws Exception {
+
+        String apiUrl = "/users/user/name/admin";
+
+        Mockito.when(userService.findByName("admin"))
+            .thenReturn(userList.get(0));
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl)
+            .accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb).andReturn(); // this could throw an exception
+        String tr = r.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList.get(0));
+
+        assertEquals("Rest API Returns List", er, tr);
     }
 
     @Test
-    public void getUserLikeName() {
+    public void getUserLikeName() throws Exception{
+        String apiUrl = "/users/user/name/like/cin";
+
+        Mockito.when(userService.findByNameContaining(any(String.class)))
+            .thenReturn(userList);
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl)
+            .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult r = mockMvc.perform(rb)
+            .andReturn();
+        String tr = r.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList);
+
+        assertEquals("Rest API Returns List", er, tr);
     }
 
     @Test
-    public void addNewUser() {
+    public void addNewUser() throws Exception{
+
+        String apiUrl = "/users/user";
+
+        User u1 = new User();
+        u1.setUserid(100);
+        u1.setUsername("tiger");
+        u1.setPassword("ILuvM4th!");
+        u1.setPrimaryemail("tiger@home.local");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String userString = mapper.writeValueAsString(u1);
+
+        Mockito.when(userService.save(any(User.class)))
+            .thenReturn(u1);
+
+        RequestBuilder rb = MockMvcRequestBuilders.post(apiUrl)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(userString);
+
+        mockMvc.perform(rb)
+            .andExpect(status().isCreated())
+            .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
-    public void updateFullUser() {
+    public void updateFullUser() throws Exception{
+
+        String apiUrl = "/users/user/{userid}";
+
+        User u1 = new User();
+        u1.setUserid(100);
+        u1.setUsername("tigerUpdated");
+        u1.setPrimaryemail("home@local.home");
+        u1.setPassword("ILuvM4th!");
+
+        Mockito.when(userService.update(u1, 100))
+            .thenReturn(userList.get(0));
+
+        ObjectMapper mapper = new ObjectMapper();
+        String userString = mapper.writeValueAsString(u1);
+
+        RequestBuilder rb = MockMvcRequestBuilders.put(apiUrl, 100L)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(userString);
+
+        mockMvc.perform(rb)
+            .andExpect(status().is2xxSuccessful())
+            .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
@@ -45,6 +284,15 @@ public class UserControllerTest {
     }
 
     @Test
-    public void deleteUserById() {
+    public void deleteUserById() throws Exception{
+
+        String apiUrl = "/users/user/{userid}";
+
+        RequestBuilder rb = MockMvcRequestBuilders.delete(apiUrl, "3")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(rb)
+            .andExpect(status().is2xxSuccessful())
+            .andDo(MockMvcResultHandlers.print());
     }
 }
--- usermodel/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
+++ usermodel/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
@@ -1,15 +1,49 @@
 package com.lambdaschool.usermodel.services;
 
+import com.lambdaschool.usermodel.UserModelApplication;
+import com.lambdaschool.usermodel.models.Role;
+import com.lambdaschool.usermodel.models.User;
+import com.lambdaschool.usermodel.models.UserRoles;
+import com.lambdaschool.usermodel.models.Useremail;
+import com.lambdaschool.usermodel.repository.UserRepository;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserModelApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserServiceImplTest {
+
+    @Autowired
+    private UserService userService;
 
     @Before
     public void setUp() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+
+//        List<User> myList = userService.findAll();
+//        for (User u : myList){
+//            System.out.println(u.getUserid() + " " + u.getUsername());
+//        }
+//        4 admin
+//        7 cinnamon
+//        11 barnbarn
+//        13 puttat
+//        14 misskitty
     }
 
     @After
@@ -17,34 +51,74 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void findUserById() {
+    public void a_findUserById() {
+
+        assertEquals("admin", userService.findUserById(4).getUsername());
     }
 
     @Test
-    public void findByNameContaining() {
+    public void b_findByNameContaining() {
+
+        assertEquals(1, userService.findByNameContaining("ad").size());
     }
 
     @Test
-    public void findAll() {
+    public void c_findAll() {
+
+        assertEquals(5, userService.findAll().size());
     }
 
     @Test
-    public void delete() {
+    public void x_delete() {
+
+        userService.delete(13);
+        assertEquals(5, userService.findAll().size());
     }
 
     @Test
-    public void findByName() {
+    public void d_findByName() {
+
+        assertEquals("admin", userService.findByName("admin").getUsername());
     }
 
     @Test
-    public void save() {
+    public void e_save() {
+
+        Role r2 = new Role("user");
+        r2.setRoleid(2);
+
+        User u2 = new User("tiger", "ILuvMath!", "tiger@school.lambda");
+        u2.getRoles().add(new UserRoles(u2, r2));
+        u2.getUseremails()
+            .add(new Useremail(u2, "tiger@tiger.local"));
+        User savedUser = userService.save(u2);
+        assertEquals("tiger", savedUser.getUsername());
+        assertEquals("tiger@school.lambda", savedUser.getPrimaryemail());
+        assertEquals(1, savedUser.getRoles().size());
+        assertEquals(1, savedUser.getUseremails().size());
+    }
+
+    @Transactional
+    @Test
+    public void f_update() {
+
+        Role r2 = new Role("user");
+        r2.setRoleid(2);
+
+        User u2 = new User("tiger", "ILuvMath!", "tiger@school.lambda");
+        u2.getRoles().add(new UserRoles(u2, r2));
+        u2.getUseremails()
+            .add(new Useremail(u2, "tiger@tiger.local"));
+        User updatedUser = userService.update(u2, 15);
+        assertEquals("tiger", updatedUser.getUsername());
+        assertEquals("tiger@school.lambda", updatedUser.getPrimaryemail());
+        assertEquals(1, updatedUser.getRoles().size());
+        assertEquals(1, updatedUser.getUseremails().size());
     }
 
     @Test
-    public void update() {
-    }
-
-    @Test
-    public void deleteAll() {
+    public void z_deleteAll() {
+        userService.deleteAll();
+        assertEquals(0, userService.findAll().size());
     }
 }
